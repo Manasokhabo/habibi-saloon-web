e2da825b8eaf316ebd9bf05fb692efddb5ec6381
Manasokhabo <102599901+Manasokhabo@users.noreply.github.com>
2026-02-16 09:04:44 -0800

    Delete services directory

--- services/localDataService.ts
+++
@@ -1,8 +0,0 @@
-// Deprecated: Switching back to Firebase for cloud persistence as requested.
-export const localDataService = {
-  getCurrentUser: () => null,
-  logout: () => {},
-  signIn: () => { throw new Error("Use Firebase Service"); },
-  signUp: () => { throw new Error("Use Firebase Service"); },
-  addBooking: () => { throw new Error("Use Firebase Service"); }
-};
